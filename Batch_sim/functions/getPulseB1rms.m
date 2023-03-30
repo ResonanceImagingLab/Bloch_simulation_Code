2238b116391a2413f3a88c963283b93bb4ed48b3
Christopher Rowley <chris_rowley18@hotmail.com>
2023-03-30 14:28:29 -0400

    Update getPulseB1rms.m
    modify to use qMRlab

--- Batch_sim/functions/getPulseB1rms.m
+++ Batch_sim/functions/getPulseB1rms.m
@@ -1,64 +1,34 @@
 function B1rms = getPulseB1rms(B1, pulseDur)
 
-% I have only computed for one pulse shape, other can be done by going
-% through the code commented out at the bottom.
+% This has been updated to take in the satFlipAngle, and then compute the
+% B1rms using qMRlab pulse functions. To do so requires:
+% 1. flip angle of the saturation pulse
+% 2. duration of the saturation pulse (in seconds)
+% 3. shape of the saturation pulse -> see qMRlab's 'GetPulse.m'
 
-% using equation 3 from: A strategy to reduce the sensitivity of inhomogeneous
-% magnetization transfer ( ihMT ) imaging to radiofrequency transmit field 
-% variations at 3T. Soustelle et al., 2021. MRM.
-
-% requires qMRlab for getting values. 
-% Pulse dur must be in seconds!
-
-% handle whether B1 is input as microtesla or tesla
-if B1 > 1e-4
-    B1 = B1./1e6;
-end
-
-gam = 42.58e6; % Hz/T
+% The exported B1rms is in microTesla.
 
 
-%% Calculations have been done for hann pulses length 0.768, 1.024, 1.28 and 2.048ms.
-
-if pulseDur == 0.768e-3
-    Integral_b1Norm = 3.8e-4;
-    
-elseif pulseDur == 1.024e-3
-    Integral_b1Norm = 5.067e-4;
-
-elseif pulseDur == 1.28e-3
-    Integral_b1Norm = 6.334e-4;
-
-elseif pulseDur == 2.048e-3
-    Integral_b1Norm = 1.0134e-3;
-
+% Set up custom adjustments to pulses. Use with qMRlabs 'ViewPulse.m'
+if strcmp(SatPulseShape, 'gausshann')
+    PulseOpt.bw = 0.0002/pulseDur;
 else
-    error('this pulse duration has not been calculated. Open function and look to bottom to calculate');
+    PulseOpt = [];
 end
-sqrtP2 = 0.6085;
 
-MTflipAngle = B1.*42.58e6.*pulseDur.*360;
-Amplitude = MTflipAngle./(360.*gam.*Integral_b1Norm); % derived from qMRlab code
-B1rms = sqrtP2 .* Amplitude;
+delta = 5000; % not important for this. 
+gam = 42.576;
+
+tSat = 0:pulseDur/200:pulseDur;
+satPulse = GetPulse(satFlipAngle, delta, pulseDur, SatPulseShape, PulseOpt);
+
+
+B1_time = satPulse.omega(tSat)/(2*pi*gam);
+P3 = (1/pulseDur) * trapz(tSat,B1_time.^2);
+B1rms = sqrt(P3);
 
 
 
-% %% For other shapes and durations: solve for P2 and integrals:
-% % Use qMRlab to be able to explor different pulse shapes:
-% MTflipAngle = 600; % this value doesn't matter for this section
-% delta = 8000; % neither does this.
-% pulseDur = 2.048e-3;
-% SatPulseShape = 'gausshann';
-% PulseOpt.bw = 0.3./pulseDur; % override default Hann pulse .
-% HsatPulse = GetPulse(MTflipAngle, delta, pulseDur, SatPulseShape, PulseOpt);
-% Trf = HsatPulse.Trf;
-% t = 0:Trf/1000:Trf;
-% y = HsatPulse.('b1')(t);
-% %plot(t,y); hold on;
-% 
-% Integral_b1Norm = trapz(t,y)
-% integSq = trapz(t,y.^2);
-% p2 =  integSq/pulseDur;
-% sqrtP2 = sqrt(p2)
+
 
 
